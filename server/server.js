4c73151a55c0c21f9edd645851bea122a7b1132b
Daniel Hernandez <daniel56hdz@gmail.com>
2017-09-08 13:47:28 -0500

    created a database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
 });
 
+
+
 connection.connect(function (err) {
     if (err) throw err
     console.log('You are now connected...');
+    connection.query('DROP DATABASE IF EXISTS node_test', (err, results) => {
+        if (err) throw err
+        console.log('Database dropped');
+    });
+    connection.query('CREATE DATABASE IF NOT EXISTS node_test', (err, results) => {
+        if (err) throw err
+        console.log('Database created');
+    });
+    connection.end(function(err) {
+        if (err) throw err
+        console.log('You have disconnected');
+      });
 });

2a6ec0f5036eb06ee0a5ef759ae3b4a1b17c6f04
Daniel Hernandez <daniel56hdz@gmail.com>
2017-09-08 18:07:48 -0500

    add GET /todos and GET /todos/:username requests

--- server/server.js
+++ server/server.js
@@ -1,26 +1,46 @@
+const express = require('express');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
+    database: 'node_test_db'
 });
-
-
+const app = express();
+const port = process.env.PORT || 3000;
 
 connection.connect(function (err) {
     if (err) throw err
     console.log('You are now connected...');
-    connection.query('DROP DATABASE IF EXISTS node_test', (err, results) => {
-        if (err) throw err
-        console.log('Database dropped');
-    });
-    connection.query('CREATE DATABASE IF NOT EXISTS node_test', (err, results) => {
-        if (err) throw err
-        console.log('Database created');
-    });
-    connection.end(function(err) {
-        if (err) throw err
-        console.log('You have disconnected');
-      });
+});
+
+app.get('/todos', (req, res) => {
+    connection.query('SELECT * FROM todos', (err, rows, fields) => {
+        connection.end();
+        if (err) {
+            return res.status(400).send(err);
+        }
+        res.send({rows})
+    })
+});
+
+app.get('/todos/:username', (req, res) => {
+    var username = req.params.username;
+    connection.query(`
+        SELECT * 
+        FROM todos
+        WHERE username LIKE '${username}'`
+        , (err, rows, fields) => {
+            connection.end();
+            if(err) {
+                return res.status(400).send(err);
+            }
+            res.send({rows})
+        }
+    )
+});
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
 });
